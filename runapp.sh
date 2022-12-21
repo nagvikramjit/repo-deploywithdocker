c4682b3451512383a3889f49d7ce54047a2fd3e6
nagvikramjit <vikramjit.nag@gmail.com>
2022-12-21 20:29:17 +0530

    modified runapp

--- runapp.sh
+++ runapp.sh
@@ -1,8 +1,9 @@
 # echo 'killing PID on port 3000 if already exists'
 # var=`lsof -Fp -i:3000 | head -n 1`
 # if [ ! -z "$var" ]; then kill -9 ${var##p}; fi
-sudo apt-get update
-sudo apt-get install python3-pip -y
-pip3 install -r requirements.txt
+# sudo apt-get update
+# sudo apt-get install python3-pip -y
+# pip3 install -r requirements.txt
 # nohup python3 main.py > app_log.txt 2>&1 &
+pip3 install -r requirements.txt
 python3 main.py
